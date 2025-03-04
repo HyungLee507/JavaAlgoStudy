c5b1fca2a687e024fa11305cf52ba38b303b8fe1
g1767g <g1767g@naver.com>
2025-03-04 23:23:11 +0900

    [BOJ] 17092 / 색칠 공부 / 풀이 참고 - 언어 : JAVA - 메모리 : 385120 KB - 시간 : 1900 ms

---
+++ 7주차/gits79/Main_색칠공부_17092.java
@@ -0,0 +1,25 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_색칠공부_17092 {
+
+    static int H, W, N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        
+
+
+
+    }
+}
